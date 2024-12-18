085e7ae53b735bde2980792ed41c0743e2be51cd
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2024-12-18 18:13:39 -0300

    fix format

--- scene/src/utils/promise-utils.ts
+++ scene/src/utils/promise-utils.ts
@@ -1,4 +1,3 @@
-
 // export async function waitOrTimeout<T>(func: () => Promise<T>, timeout: number, defaultValue: T): Promise<T> {
 //   const timeoutPromise = new Promise<T>((resolve) => setTimeout(() => resolve(defaultValue), timeout))
 //   return Promise.race([func(), timeoutPromise])
