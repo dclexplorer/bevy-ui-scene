8dcda421aa68b15d45610b56bb6e42f8bb10fb6f
pabloes <estornut@gmail.com>
2025-03-03 14:19:49 +0100

    test: backpack: fix unit test for getURNWithoutTokenId

--- scene/src/utils/URN-utils.ts
+++ scene/src/utils/URN-utils.ts
@@ -1,10 +1,14 @@
 import type {URN, URNWithoutTokenId} from "./definitions";
 
-const urnWithoutTokenIdMemo:Record<URN, URNWithoutTokenId> = {} // TODO consider using Map if possible for performance improvement because long keys
+const urnWithoutTokenIdMemo: Record<URN, URNWithoutTokenId> = {} // TODO consider using Map if possible for performance improvement because long keys
 
-export function getURNWithoutTokenId(urn:URN|null, avoidCache:boolean = false):URN|null{
-    if(urn === null) return null
-    if(urnWithoutTokenIdMemo[urn] !== undefined && !avoidCache) return urnWithoutTokenIdMemo[urn];
-    urnWithoutTokenIdMemo[urn] = (urn.includes(":off-chain:") || urn.split(":").length < 6 ? urn : urn.replace(/^(.*):[^:]+$/, "$1")) as URNWithoutTokenId
+export function getURNWithoutTokenId(urn: URN | null, avoidCache: boolean = false): URN | null {
+    if (urn === null) return null;
+    if (urnWithoutTokenIdMemo[urn] !== undefined && !avoidCache) return urnWithoutTokenIdMemo[urn];
+    if (!urn.includes(":item:") || urn.split(":").length < 6) {
+        urnWithoutTokenIdMemo[urn] = urn;
+    } else {
+        urnWithoutTokenIdMemo[urn] = urn.replace(/^(.*):[^:]+$/, "$1") as URNWithoutTokenId;
+    }
     return urnWithoutTokenIdMemo[urn];
 }
--- test/unit/getURNWithoutTokenId.test.ts
+++ test/unit/getURNWithoutTokenId.test.ts
@@ -1,26 +1,27 @@
-import {getURNWithoutTokenId} from "../../scene/src/utils/URN-utils";
-import {URN, URNWithoutTokenId} from "../../scene/src/utils/definitions";
+import { expect } from 'chai';
+import { getURNWithoutTokenId } from "../../scene/src/utils/URN-utils";
+import { URN, URNWithoutTokenId } from "../../scene/src/utils/definitions";
 
-describe("getURNWithoutTokenId", () => {
+describe("getURNWithoutTokenId1", () => {
     it("should return null if the URN is null", () => {
-        expect(getURNWithoutTokenId(null)).toBeNull();
+        expect(getURNWithoutTokenId(null)).to.be.null;
     });
 
     it("should return the same URN if it's off-chain", () => {
         const urn: URN = "urn:decentraland:off-chain:base-avatars:generic-avatar";
-        expect(getURNWithoutTokenId(urn)).toBe(urn);
+        expect(getURNWithoutTokenId(urn)).to.equal(urn);
     });
 
     it("should return the URN without the tokenId if it's on-chain", () => {
         const urn: URN = "urn:decentraland:ethereum:collections-v1:0x1234:item:56";
         const expected: URNWithoutTokenId = "urn:decentraland:ethereum:collections-v1:0x1234:item";
-        expect(getURNWithoutTokenId(urn)).toBe(expected);
+        expect(getURNWithoutTokenId(urn)).to.equal(expected);
     });
 
     it("should not break the URN when executed twice", () => {
         const urn: URN = "urn:decentraland:ethereum:collections-v1:0x1234:item:56";
         const result1 = getURNWithoutTokenId(urn);
         const result2 = getURNWithoutTokenId(result1 as URN);
-        expect(result1).toBe(result2);
+        expect(result2).to.equal("urn:decentraland:ethereum:collections-v1:0x1234:item");
     });
 });
