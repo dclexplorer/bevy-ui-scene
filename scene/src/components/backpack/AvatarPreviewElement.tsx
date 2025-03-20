4dc7daa1b44e4ef22099acec52d6b2aa3c228126
pabloes <estornut@gmail.com>
2025-03-20 10:08:01 +0100

    feat: backpack WIP: add more zoom sensibility

--- scene/src/components/backpack/AvatarPreviewElement.tsx
+++ scene/src/components/backpack/AvatarPreviewElement.tsx
@@ -87,7 +87,7 @@ export function AvatarPreviewElement(): ReactElement {
           >
             <UiEntity
               uiTransform={{
-                height: 10000 * canvasScaleRatio,
+                height: 6000 * canvasScaleRatio,
                 width: '100%'
               }}
             ></UiEntity>
