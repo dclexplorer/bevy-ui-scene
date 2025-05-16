3b848feaaef9b2ab7e961673688a6b26c820023d
pabloes <estornut@gmail.com>
2025-05-16 16:13:29 +0200

    feat: outfits WIP: FilledOutfitSlot as function

--- scene/src/ui-classes/main-menu/backpack-page/OutfitsCatalog.tsx
+++ scene/src/ui-classes/main-menu/backpack-page/OutfitsCatalog.tsx
@@ -75,16 +75,6 @@ export const OutfitsCatalog = (): ReactElement => {
       }}
     >
       {viewSlots.map((viewSlot, index: number) => {
-        const uvs = getUvsFromSprite({
-          spriteDefinition: {
-            spriteSheetWidth: 4,
-            spriteSheetHeight: 3,
-            x: (index % 4) + 0.2,
-            y: Math.floor(index / 4),
-            w: 145 / 250,
-            h: 1
-          }
-        })
         return (
           <UiEntity
             key={index}
@@ -176,41 +166,11 @@ export const OutfitsCatalog = (): ReactElement => {
                 <EmptySlot slotIndex={index} />
               )}
 
-              {!isEmptySlot(viewSlot) && (
-                <UiEntity
-                  uiTransform={{
-                    positionType: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                    alignSelf: 'center',
-                    position: { left: '0%' },
-                    zIndex: 9,
-                    pointerFilter: 'none'
-                  }}
-                  onMouseDown={() => {
-                    if (
-                      state.lastTimeClick + DOUBLE_CLICK_DELAY > Date.now() &&
-                      state.lastElementClick === index
-                    ) {
-                      equipOutfit(viewSlot).catch(console.error)
-                    } else {
-                      state.lastTimeClick = Date.now()
-                      state.lastElementClick = index
-                      state.selectedIndex = index
-                    }
-                  }}
-                  uiBackground={{
-                    textureMode: 'stretch',
-                    /* texture: {
-                    src: 'assets/images/mock_outfits_for_uvs_2.png'
-                  }, */
-                    videoTexture: {
-                      videoPlayerEntity: outfitsCameraEntity
-                    },
-                    uvs
-                  }}
-                />
-              )}
+              {!isEmptySlot(viewSlot) &&
+                FilledOutfitSlot({
+                  viewSlot: viewSlot as OutfitDefinition,
+                  slotIndex: index
+                })}
               {isEmptySlot(viewSlot) && isFirstAvailableSlot(index) && (
                 <BuyNameSlot />
               )}
@@ -242,6 +202,58 @@ export const OutfitsCatalog = (): ReactElement => {
     </UiEntity>
   )
 
+  function FilledOutfitSlot({
+    viewSlot,
+    slotIndex
+  }: {
+    viewSlot: OutfitDefinition
+    slotIndex: number
+  }) {
+    return (
+      <UiEntity
+        uiTransform={{
+          positionType: 'absolute',
+          width: '100%',
+          height: '100%',
+          alignSelf: 'center',
+          position: { left: '0%' },
+          zIndex: 9,
+          pointerFilter: 'none'
+        }}
+        onMouseDown={() => {
+          if (
+            state.lastTimeClick + DOUBLE_CLICK_DELAY > Date.now() &&
+            state.lastElementClick === slotIndex
+          ) {
+            equipOutfit(viewSlot).catch(console.error)
+          } else {
+            state.lastTimeClick = Date.now()
+            state.lastElementClick = slotIndex
+            state.selectedIndex = slotIndex
+          }
+        }}
+        uiBackground={{
+          textureMode: 'stretch',
+          /* texture: {
+        src: 'assets/images/mock_outfits_for_uvs_2.png'
+      }, */
+          videoTexture: {
+            videoPlayerEntity: outfitsCameraEntity
+          },
+          uvs: getUvsFromSprite({
+            spriteDefinition: {
+              spriteSheetWidth: 4,
+              spriteSheetHeight: 3,
+              x: (slotIndex % 4) + 0.2,
+              y: Math.floor(slotIndex / 4),
+              w: 145 / 250,
+              h: 1
+            }
+          })
+        }}
+      />
+    )
+  }
   async function equipOutfit(viewSlot: OutfitDefinition | null): Promise<void> {
     store.dispatch(
       updateAvatarBase({
