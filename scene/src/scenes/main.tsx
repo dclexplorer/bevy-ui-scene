a65825b7316678eb0f48c6781e41095739f26f66
pabloes <estornut@gmail.com>
2025-10-13 13:55:16 +0200

    fix: format-fix

--- scene/src/scenes/main.tsx
+++ scene/src/scenes/main.tsx
@@ -5,7 +5,6 @@ import { loadSettingsFromExplorer } from '../state/settings/actions'
 import { store } from '../state/store'
 import { executeTask } from '@dcl/sdk/ecs'
 import { sleep } from '../utils/dcl-utils'
-import { updateHudStateAction } from '../state/hud/actions'
 
 let gameInstance: GameController
 
