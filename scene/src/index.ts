485852ff3e6a979b71326c9d64eeff7d33d773a7
pabloes <estornut@gmail.com>
2025-11-14 12:22:14 +0100

    disable error popups

--- scene/src/index.ts
+++ scene/src/index.ts
@@ -1,18 +1,8 @@
 import { init } from './scenes/main'
 import { initRealmProviderChangeListener } from './service/realm-change'
 import { initAvatarTags } from './components/avatar-tags/avatar-name-tag-3d'
-import { showErrorPopup } from './service/error-popup-service'
 
 export function main(): void {
-  const _consoleError = console.error
-  console.error = (...args) => {
-    try {
-      _consoleError('console.error', ...args)
-      showErrorPopup(`ConsoleError from System scene: ${args.join('\n')}`)
-    } catch (error) {
-      console.error(error, ...args)
-    }
-  }
   initRealmProviderChangeListener()
 
   init(false).catch((e) => {
